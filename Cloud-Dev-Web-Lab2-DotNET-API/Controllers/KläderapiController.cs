56b8c0de789f945692998104d533881dcf7917f2
Libre255 <briangbv@outlook.com>
2023-02-20 12:44:33 +0100

    Add project files.

---
+++ Cloud-Dev-Web-Lab2-DotNET-API/Controllers/KläderapiController.cs
@@ -0,0 +1,40 @@
+﻿using Cloud_Dev_Web_Lab2_DotNET_API.Modules;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Cloud_Dev_Web_Lab2_DotNET_API.Controllers
+{
+    [ApiController]
+    [Route("watch?v=dwaufhewoigt4o")]
+    public class KläderapiController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using(var client = new HttpClient())
+            {
+                var endpoint = new Uri("");
+                var result = client.GetAsync(endpoint).Result;
+                var json = result.Content.ReadAsStringAsync().Result; //json result
+                return Ok(json);
+            }
+        }
+        [HttpPost]
+        public void Post(Clothes Item)
+        {
+            using(var client = new HttpClient())
+            {
+                Uri endpoint = new Uri("");
+                Clothes newClothes = new Clothes()
+                {
+                    id = 333,
+                    name = "Paourme",
+                    type = "Coat"
+                };
+                var newClothesJson = JsonConvert.SerializeObject(newClothes);
+
+
+            }
+        }
+    }
+}
---
+++ Cloud-Dev-Web-Lab2-DotNET-API/Modules/Clothes.cs
@@ -0,0 +1,9 @@
+﻿namespace Cloud_Dev_Web_Lab2_DotNET_API.Modules
+{
+    public class Clothes
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+    }
+}
